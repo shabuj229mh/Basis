e47a36f10cc40708ad9c90ab7271e1863be4d611
shabuj229mh <shabuj_229m@yahoo.com>
2015-03-06 14:37:09 +0600

    initial commit

---
+++ Circle/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circle
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            double perimeter;
+            double radius;
+            Console.WriteLine("Give me the radius:");
+            //Console.ReadLine();
+            radius = Convert.ToDouble(Console.ReadLine());
+            perimeter = 2*radius;
+            Console.WriteLine("The primeter is:"+ perimeter);
+
+            Console.ReadKey();
+        }
+
+    }
+}
